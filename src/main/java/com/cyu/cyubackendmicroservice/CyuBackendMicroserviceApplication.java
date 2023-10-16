bd55bba8db499eab0a1ba1cfb71e8aa9ac76c3a7
mengliang <13767029108@163.com>
2023-10-16 14:08:43 +0800

    Initial commit

---
+++ src/main/java/com/cyu/cyubackendmicroservice/CyuBackendMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cyu.cyubackendmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyuBackendMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyuBackendMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyu/cyubackendmicroservice/CyuBackendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyu.cyubackendmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyuBackendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

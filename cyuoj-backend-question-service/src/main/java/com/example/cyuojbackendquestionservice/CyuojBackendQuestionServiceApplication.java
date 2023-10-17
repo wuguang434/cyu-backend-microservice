00da5c63fe3c6c263361c488482cadc2e1d298a8
mengliang <13767029108@163.com>
2023-10-17 13:13:29 +0800

    题目服务

---
+++ cyuoj-backend-question-service/src/main/java/com/example/cyuojbackendquestionservice/CyuojBackendQuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyuojbackendquestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyuojBackendQuestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyuojBackendQuestionServiceApplication.class, args);
+    }
+
+}
---
+++ cyuoj-backend-question-service/src/test/java/com/example/cyuojbackendquestionservice/CyuojBackendQuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyuojbackendquestionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyuojBackendQuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

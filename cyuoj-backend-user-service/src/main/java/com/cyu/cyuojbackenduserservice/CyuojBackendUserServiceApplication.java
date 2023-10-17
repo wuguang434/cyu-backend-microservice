3bb62d9dce530d9e5297c6e36bd808196fef7cda
mengliang <13767029108@163.com>
2023-10-17 13:12:53 +0800

    用户服务:(注册,登录,用户管理)

---
+++ cyuoj-backend-user-service/src/main/java/com/cyu/cyuojbackenduserservice/CyuojBackendUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cyu.cyuojbackenduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyuojBackendUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyuojBackendUserServiceApplication.class, args);
+    }
+
+}
---
+++ cyuoj-backend-user-service/src/test/java/com/cyu/cyuojbackenduserservice/CyuojBackendUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyu.cyuojbackenduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyuojBackendUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

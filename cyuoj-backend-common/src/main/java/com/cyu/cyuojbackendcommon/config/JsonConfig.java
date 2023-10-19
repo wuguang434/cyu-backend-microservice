b54d8cc2670ebf85743d6fcce7009b897a127194
mengliang <13767029108@163.com>
2023-10-19 09:16:44 +0800

    添加 Long 转 json 精度丢失的配置

---
+++ cyuoj-backend-common/src/main/java/com/cyu/cyuojbackendcommon/config/JsonConfig.java
@@ -0,0 +1,33 @@
+package com.cyu.cyuojbackendcommon.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Spring MVC Json 配置
+ /**
+ * 自定义错误码
+ *
+ * # @author <a href="https://github.com/wuguang434">Coding boy:xlei</a>
+ */
+
+@JsonComponent
+public class JsonConfig {
+
+    /**
+     * 添加 Long 转 json 精度丢失的配置
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}

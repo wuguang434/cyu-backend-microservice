3b8d7d8844eb2e43bfceb89572d8b06ae8e14684
mengliang <13767029108@163.com>
2023-10-19 10:21:53 +0800

    文件常量

---
+++ cyuoj-backend-common/src/main/java/com/cyu/cyuojbackendcommon/constant/FileConstant.java
@@ -0,0 +1,15 @@
+package com.cyu.cyuojbackendcommon.constant;
+
+/**
+ * 文件常量
+ *
+ # @author <a href="https://github.com/wuguang434">Coding boy:xlei</a>
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://xlei.icu";
+}

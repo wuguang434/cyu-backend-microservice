da09aeabc26d287116cae8f81bd8f0e90c0f6f6e
mengliang <13767029108@163.com>
2023-10-19 10:29:07 +0800

    启动类

--- cyuoj-backend-gateway/src/main/java/com/cyu/cyuojbackendgateway/CyuojBackendGatewayApplication.java
+++ cyuoj-backend-gateway/src/main/java/com/cyu/cyuojbackendgateway/CyuojBackendGatewayApplication.java
@@ -2,8 +2,12 @@ package com.cyu.cyuojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//扫描该包将其注册为bean,需要使用的时候将其注入
+@EnableDiscoveryClient
 public class CyuojBackendGatewayApplication {
 
     public static void main(String[] args) {

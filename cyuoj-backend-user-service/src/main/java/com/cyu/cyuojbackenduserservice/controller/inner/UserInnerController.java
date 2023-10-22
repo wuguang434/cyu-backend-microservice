e7e5ed4553a3686be44a8c1cbcedd90ef2c017ac
mengliang <13767029108@163.com>
2023-10-22 15:20:26 +0800

    实现服务内部调用的一些接口(不给前端)

---
+++ cyuoj-backend-user-service/src/main/java/com/cyu/cyuojbackenduserservice/controller/inner/UserInnerController.java
@@ -0,0 +1,50 @@
+package com.cyu.cyuojbackenduserservice.controller.inner;
+
+import com.cyu.cyuojbackendmodel.model.entity.User;
+import com.cyu.cyuojbackendserviceclient.service.UserFeignClient;
+import com.cyu.cyuojbackenduserservice.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 实现服务内部调用的一些接口(不给前端)
+ */
+@RestController
+@RequestMapping("/inner")
+public class UserInnerController implements UserFeignClient {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 根据id获取用户
+     *
+     * @param userId
+     * @return
+     * @RequestParam常用于Get请求,则该参数必须传递
+     */
+    @Override
+    @GetMapping("/get/id")
+    public User getById(@RequestParam("userId") long userId) {
+        return userService.getById(userId);
+    }
+
+    /**
+     * 根据id获取用户列表
+     *
+     * @param idList
+     * @return
+     */
+    @Override
+    @GetMapping("/get/ids")
+    public List<User> listByIds(@RequestParam("idList") Collection<Long> idList) {
+        return userService.listByIds(idList);
+    }
+
+}

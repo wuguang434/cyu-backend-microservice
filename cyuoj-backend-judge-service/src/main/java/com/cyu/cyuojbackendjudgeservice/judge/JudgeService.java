c57fc4f54e94abdbb7d9053e1238c9f8a52b0841
mengliang <13767029108@163.com>
2023-11-05 19:19:03 +0800

    判题服务接口

---
+++ cyuoj-backend-judge-service/src/main/java/com/cyu/cyuojbackendjudgeservice/judge/JudgeService.java
@@ -0,0 +1,15 @@
+package com.cyu.cyuojbackendjudgeservice.judge;
+
+import com.cyu.cyuojbackendmodel.model.entity.QuestionSubmit;
+
+/**
+ * 判题服务
+ */
+public interface JudgeService {
+    /**
+     * 判题
+     * @param questionSubmitId
+     * @return
+     */
+    QuestionSubmit doJudge(long questionSubmitId);
+}

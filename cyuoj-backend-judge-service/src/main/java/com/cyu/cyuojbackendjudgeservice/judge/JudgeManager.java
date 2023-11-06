99f5b7de8d9940baf05773d3862dd908a7ee8bfc
mengliang <13767029108@163.com>
2023-11-06 20:38:02 +0800

    判题管理(简化调用)

---
+++ cyuoj-backend-judge-service/src/main/java/com/cyu/cyuojbackendjudgeservice/judge/JudgeManager.java
@@ -0,0 +1,31 @@
+package com.cyu.cyuojbackendjudgeservice.judge;
+
+import com.cyu.cyuojbackendjudgeservice.judge.strategy.DefaultJudgeStrategy;
+import com.cyu.cyuojbackendjudgeservice.judge.strategy.JavaLanguageJudgeStrategy;
+import com.cyu.cyuojbackendjudgeservice.judge.strategy.JudgeContext;
+import com.cyu.cyuojbackendjudgeservice.judge.strategy.JudgeStrategy;
+import com.cyu.cyuojbackendmodel.model.codesandbox.JudgeInfo;
+import com.cyu.cyuojbackendmodel.model.entity.QuestionSubmit;
+import org.springframework.stereotype.Service;
+
+/**
+ * 判题管理(简化调用)
+ */
+@Service
+public class JudgeManager {
+    /**
+     * 执行判题
+     *
+     * @param judgeContext
+     * @return
+     */
+    JudgeInfo doJudge(JudgeContext judgeContext) {
+        QuestionSubmit questionSubmit = judgeContext.getQuestionSubmit();
+        String language = questionSubmit.getLanguage();
+        JudgeStrategy judgeStrategy = new DefaultJudgeStrategy();
+        if ("java".equals(language)) {
+            judgeStrategy = new JavaLanguageJudgeStrategy();
+        }
+        return judgeStrategy.doJudge(judgeContext);
+    }
+}

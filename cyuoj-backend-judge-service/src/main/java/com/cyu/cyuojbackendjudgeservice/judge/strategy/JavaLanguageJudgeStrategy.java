d0f03f1b6f7e9fe82769300f10ac63280406c031
mengliang <13767029108@163.com>
2023-11-11 12:57:39 +0800

    Java 程序判题策略

---
+++ cyuoj-backend-judge-service/src/main/java/com/cyu/cyuojbackendjudgeservice/judge/strategy/JavaLanguageJudgeStrategy.java
@@ -0,0 +1,71 @@
+package com.cyu.cyuojbackendjudgeservice.judge.strategy;
+
+import cn.hutool.json.JSONUtil;
+import com.cyu.cyuojbackendmodel.model.codesandbox.JudgeInfo;
+import com.cyu.cyuojbackendmodel.model.dto.question.JudgeCase;
+import com.cyu.cyuojbackendmodel.model.dto.question.JudgeConfig;
+import com.cyu.cyuojbackendmodel.model.entity.Question;
+import com.cyu.cyuojbackendmodel.model.enums.JudgeInfoMessageEnum;
+
+import java.util.List;
+import java.util.Optional;
+/**
+ * Java 程序判题策略
+ */
+public class JavaLanguageJudgeStrategy implements JudgeStrategy {
+    /**
+     * 执行判题
+     * @param judgeContext
+     * @return
+     */
+    @Override
+    public JudgeInfo doJudge(JudgeContext judgeContext) {
+        JudgeInfo judgeInfo = judgeContext.getJudgeInfo();
+        Long memory = Optional.ofNullable(judgeInfo.getMemory()).orElse(0L);
+        Long time = Optional.ofNullable(judgeInfo.getTime()).orElse(0L);
+        List<String> inputList = judgeContext.getInputList();
+        List<String> outputList = judgeContext.getOutputList();
+        Question question = judgeContext.getQuestion();
+        List<JudgeCase> judgeCaseList = judgeContext.getJudgeCaseList();
+        JudgeInfoMessageEnum judgeInfoMessageEnum = JudgeInfoMessageEnum.ACCEPTED;
+        JudgeInfo judgeInfoResponse = new JudgeInfo();
+
+        judgeInfoResponse.setMemory(memory);
+        judgeInfoResponse.setTime(time);
+
+        if (outputList.size() != inputList.size()) {
+            judgeInfoMessageEnum = JudgeInfoMessageEnum.WRONG_ANSWER;
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            return judgeInfoResponse;
+        }
+        for (int i = 0; i < judgeCaseList.size(); i++) {
+            JudgeCase judgeCase = judgeCaseList.get(i);
+            if (!judgeCase.getOutput().equals(outputList.get(i))) {
+                judgeInfoMessageEnum = JudgeInfoMessageEnum.WRONG_ANSWER;
+                judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+                return judgeInfoResponse;
+            }
+        }
+        //判断题目限制
+
+        String judgeConfigStr = question.getJudgeConfig();
+        JudgeConfig judgeConfig = JSONUtil.toBean(judgeConfigStr, JudgeConfig.class);
+
+        Long needMemoryLimit = judgeConfig.getMemoryLimit();
+        Long needTimeLimit = judgeConfig.getTimeLimit();
+        if (memory > needMemoryLimit) {
+            judgeInfoMessageEnum = JudgeInfoMessageEnum.MEMORY_LIMIT_EXCEEDED;
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            return judgeInfoResponse;
+        }
+        //java需要额外执行10秒
+        long JAVA_PROGRAM_TIME_COST=10000L;
+        if (time -JAVA_PROGRAM_TIME_COST> needTimeLimit) {
+            judgeInfoMessageEnum = JudgeInfoMessageEnum.TIME_LIMIT_EXCEEDED;
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            return judgeInfoResponse;
+        }
+        judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+        return judgeInfoResponse;
+    }
+}

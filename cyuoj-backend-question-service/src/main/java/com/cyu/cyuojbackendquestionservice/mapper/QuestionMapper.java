bcbc9a9828ea2cfec93999ec046487d58932a876
mengliang <13767029108@163.com>
2023-11-26 16:19:48 +0800

    针对表【question(题目)】的数据库操作Mapper

---
+++ cyuoj-backend-question-service/src/main/java/com/cyu/cyuojbackendquestionservice/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.cyu.cyuojbackendquestionservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cyu.cyuojbackendmodel.model.entity.Question;
+
+/**
+* @author 12100
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2023-09-09 15:36:16
+* @Entity com.xlei.cyoj3.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+

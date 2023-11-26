773e68366e4dea3c8c37d0bd593aa6fcf9a5ca0a
mengliang <13767029108@163.com>
2023-11-26 16:20:08 +0800

    针对表【question_submit(题目提交)】的数据库操作Mapper

---
+++ cyuoj-backend-question-service/src/main/java/com/cyu/cyuojbackendquestionservice/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.cyu.cyuojbackendquestionservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cyu.cyuojbackendmodel.model.entity.QuestionSubmit;
+
+/**
+* @author 12100
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2023-09-09 15:40:34
+* @Entity com.xlei.cyoj3.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+

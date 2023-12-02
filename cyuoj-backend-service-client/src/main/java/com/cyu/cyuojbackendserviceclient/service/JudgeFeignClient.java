0a9e5e0c27f00230877ca7cb143c4b6f1f8b695c
mengliang <13767029108@163.com>
2023-12-02 20:48:49 +0800

    判题服务

---
+++ cyuoj-backend-service-client/src/main/java/com/cyu/cyuojbackendserviceclient/service/JudgeFeignClient.java
@@ -0,0 +1,21 @@
+package com.cyu.cyuojbackendserviceclient.service;
+
+import com.cyu.cyuojbackendmodel.model.entity.QuestionSubmit;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 判题服务
+ */
+@FeignClient(name = "cyuoj-backend-judge-service", path = "/api/judge/inner")
+public interface JudgeFeignClient {
+    /**
+     * 判题
+     *
+     * @param questionSubmitId
+     * @return
+     */
+    @PostMapping("/do")
+    QuestionSubmit doJudge(@RequestParam("questionSubmitId") long questionSubmitId);
+}

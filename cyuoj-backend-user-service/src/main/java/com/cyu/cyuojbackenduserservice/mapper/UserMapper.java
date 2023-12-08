0924f7b0d3a387e5c002f25c4d6b236d8bc20664
mengliang <13767029108@163.com>
2023-12-08 21:05:23 +0800

    用户数据库操作

---
+++ cyuoj-backend-user-service/src/main/java/com/cyu/cyuojbackenduserservice/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.cyu.cyuojbackenduserservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cyu.cyuojbackendmodel.model.entity.User;
+
+
+/**
+ * 用户数据库操作
+ *
+ # @author <a href="https://github.com/wuguang434">Coding boy:xlei</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
